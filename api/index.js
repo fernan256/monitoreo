d2b2926ee79317c7aa50d7d1cac45151baebdbd8
Diego <diego.mayorga@lopsa.com.ar>
2015-09-02 00:37:19 -0300

    feat(index): test server in heroku

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ var Hapi = require('hapi');
 var server = new Hapi.Server();
 var sys = require('sys')
 var exec = require('child_process').exec;
+var shellParser = require('node-shell-parser');
 var child;
 
 server.connection({
@@ -12,22 +13,52 @@ server.connection({
 
 server.route({
     method: 'GET',
-    path: '/hello',
+    path: '/process',
     handler: function (request, reply) {
+			
 
 			//function puts(error, stdout, stderr) { sys.puts(stdout) }
 			//exec("ls -la", puts);
-			child = exec("ps faxu", function (error, stdout, stderr) {
-				console.log(stdout);
-				console.log(stderr);
-				reply(stdout);
-  			sys.print('stdout: ' + stdout);
-  			sys.print('stderr: ' + stderr);
-  			if (error !== null) {
-					console.log('exec error: ' + error);
-				}
-			});
+			//function execute (command, callback) {
+			//	exec(command, function(error, stdout, stderr){ callback(stdout); });
+				child = exec("ps faxu", function (error, stdout, stderr) {
+				var result = shellParser(stdout);
+				reply(result);
+  		//	sys.print('stdout: ' + stdout);
+  		//	sys.print('stderr: ' + stderr);
+  				//if (error !== null) {
+					//	console.log('exec error: ' + error);
+					//}
+				});	
+			//}
+			
+		
+
+//child.kill("SIGKILL")
+
+			//execFile("ls", ["-lF", "/usr"], null, function (err, stdout, stderr) {
+  		//	console.log("execFileSTDOUT:", JSON.stringify(stdout))
+  		//	console.log("execFileSTDERR:", JSON.stringify(stderr))
+			//})
     }
+});
+
+server.route({
+    method: 'POST',
+    path: '/delete/{pid}',
+    handler: function (request, reply) {
+
+			console.log(request.params);
+		}
+});
+
+server.route({
+    method: 'POST',
+    path: '/reprioritize/{pid}',
+    handler: function (request, reply) {
+
+			console.log(request.params);
+	}
 });
 
 server.start(function() {
